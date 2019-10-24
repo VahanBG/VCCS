da121b033ce48356df1e24d1c3ec83f6edb51ec8
VahanBG <vahan.boghossian@epita.fr>
2019-10-24 16:09:53 +0200

    Cleanup the code even more. I hope I didn't break anything

--- dl.lua
+++ dl.lua
@@ -9,35 +9,33 @@ end
 
 local options = {}
 
-local indices = {}
+local args = {}
 
 for i = 1, #argv do
 	if string.sub(argv[i], 1, 1) == '-' then
 		options[#options + 1] = string.sub(argv[i], 2, #argv[i])
-		indices[#indices + 1] = i
+	else
+		args[#args + 1] = argv[i]
 	end
 end
 
-for i = 1, #indices do
-	table.remove(argv, indices[i])
+if #args > 2 then
+	print(usage)
+	return
 end
 
 local overwrite = false
 
 for i = 1, #options do
 	if string.sub(options[i], 1, 1) == '-' then
-		local option = string.lower(string.sub(options[i], 2, #options[i]))
-
-		if option == "overwrite" then
+		if string.lower(string.sub(options[i], 2, #options[i])) == "overwrite" then
 			overwrite = true
 		else
 			print(usage)
 			return
 		end
 	else
-		local option = string.lower(string.sub(options[i], 1, 1))
-
-		if option == 'o' then
+		if string.lower(string.sub(options[i], 1, 1)) == 'o' then
 			overwrite = true
 		else
 			print(usage)
@@ -46,14 +44,14 @@ for i = 1, #options do
 	end
 end
 
-local http_handle = http.get(argv[1])
+local http_handle = http.get(args[1])
 
 if not http_handle then
 	error("dl: Could not reach file")
 end
 
-if fs.exists(argv[2]) and (not overwrite) then
-	write("dl: File with this name already exists. Overwrite \"" .. argv[2] .. "\"? ")
+if fs.exists(args[2]) and (not overwrite) then
+	write("dl: File with this name already exists. Overwrite \"" .. args[2] .. "\"? ")
 	local input = string.lower(read())
 	if input ~= 'y' and input ~= "yes" then
 		http_handle.close()
@@ -61,6 +59,6 @@ if fs.exists(argv[2]) and (not overwrite) then
 	end
 end
 
-local sw = fs.open(argv[2], 'w')
+local sw = fs.open(args[2], 'w')
 sw.write(http_handle.readAll())
 sw.close()
